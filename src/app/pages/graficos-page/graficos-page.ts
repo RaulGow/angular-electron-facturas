bf699ff3bf47a8c6b96183bb3b3bf804d1cd8b2a
Raul Martinez <raul.martinezm@nateevo.com>
2026-01-20 22:32:46 +0100

    add chart.js ng2-charts en graficos de prueba

--- src/app/pages/graficos-page/graficos-page.ts
+++ src/app/pages/graficos-page/graficos-page.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { formatDate } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { ChartConfiguration, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'graficos-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, BaseChartDirective],
+  providers: [provideCharts(withDefaultRegisterables())],
   templateUrl: './graficos-page.html',
   styleUrls: ['./graficos-page.scss'],
 })
@@ -13,13 +16,30 @@ export class GraficosPage implements OnInit, OnDestroy {
 
   today = new Date();
 
-  ngOnInit() {
-    // ngOninit
-  }
+  // 3. Configuración de los DATOS
+  public lineChartData: ChartConfiguration['data'] = {
+    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56],
+        label: 'Ventas 2026',
+        borderColor: '#42A5F5',
+        backgroundColor: 'rgba(66, 165, 245, 0.2)',
+        fill: 'origin',
+      }
+    ]
+  };
 
-  ngOnDestroy() {
-    //destroy
-  }
+  public lineChartOptions: ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  // Forzamos el tipo exacto
+  public lineChartType: ChartType = 'line';
+
+  ngOnInit() { }
+  ngOnDestroy() { }
 
   get fechaFormateada(): string {
     // 1. Obtenemos la fecha en formato: "martes, 20 de enero de 2026"
